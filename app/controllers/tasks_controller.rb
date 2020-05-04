9bef0568c63e6a0076d34b904ca3b2bac7582294
takafumi <poplalala8@gmail.com>
2020-05-04 10:47:15 +0000

    kadai-tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,62 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    set_task
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+        
+    if @task.save
+      flash[:success] = "Taskが正常に投稿されました"
+      redirect_to @task
+    else
+      flash[:danger] = "Taskが投稿されませんでした"
+      render :new
+    end
+  end
+
+  def edit
+    set_task
+  end
+
+  def update
+    set_task
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    set_task
+    @task.destroy
+    
+    flash[:success] ="Taskは正常に削除されました"
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+end
