9f33312d36560e7cfebf34e9ca5f42fe0b3f5cef
takafumi <poplalala8@gmail.com>
2020-05-11 13:03:32 +0000

    kadai-tasklist-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
   root to: "tasks#index"
   
   get "signup", to: "users#new"
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show, :create]
   
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
